4c3a5b5aeaf2dfb941c16cf2e36e20daaa2c076f
Ramon K. Shin <gml4009@gmail.com>
2022-09-11 21:14:13 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './header/Header'
+import MainContext from "./context/Context";
+const App : React.FC = () => {
+    return (
+        <div className="App">
+            <Header/>
+
+            <div className={'left'}>
+                test
+            </div>
+            <div className={'context'}>
+                <MainContext/>
+            </div>
+            <div className={'right'}>
+                right
+            </div>
+
+            <footer>
+                test
+            </footer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/context/Context.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContext : React.FC = () => {
+    return (
+      <div>
+
+      </div>
+    );
+}
+
+export default MainContext;
